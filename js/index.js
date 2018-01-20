d20a96eeedd1513d4d4c372093e8d626893c44ae
Georgia Wang <georgiawang5332@gmail.com>
2018-01-20 23:50:25 +0800

    Requires test: nav part needs to be changed again

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// Script to open and close sidebar
+// function c_open() {
+//     document.getElementById("mySidebar").style.display = "block";
+//     document.getElementById("myOverlay").style.display = "block";
+// }
+//
+// function c_close() {
+//     document.getElementById("mySidebar").style.display = "none";
+//     document.getElementById("myOverlay").style.display = "none";
+// }
+
+$(document).ready(function(){
+    // $(".openItem").find('.i_menu').css({"color":"#fff"});
+    // $(".openItem").find('.remove-item').addClass("hidden");
+    $(".openItem").click(function(){
+        $(this).find('.fa-bars').toggleClass("hidden");
+        $(this).find('.fa-remove').toggleClass("hidden");//.css({"color":"#fff"})
+        //全螢幕與手機螢幕畫面
+        $(this).parents('header').siblings('.open_ul').toggleClass("show");
+    })
+});
+
+
+
