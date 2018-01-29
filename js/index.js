e1c14b05facf23eae6149c43ef501a137e431f00
Georgia Wang <georgiawang5332@gmail.com>
2018-01-29 19:04:28 +0800

    Change the phone menu section

--- js/index.js
+++ js/index.js
@@ -9,16 +9,21 @@
 //     document.getElementById("myOverlay").style.display = "none";
 // }
 
-$(document).ready(function(){
-    // $(".openItem").find('.i_menu').css({"color":"#fff"});
-    // $(".openItem").find('.remove-item').addClass("hidden");
-    $(".openItem").click(function(){
-        $(this).find('.fa-bars').toggleClass("hidden");
-        $(this).find('.fa-remove').toggleClass("hidden");//.css({"color":"#fff"})
-        //全螢幕與手機螢幕畫面
+// $(document).ready(function(){
+//     // $(".openItem").find('.i_menu').css({"color":"#fff"});
+//     // $(".openItem").find('.remove-item').addClass("hidden");
+//     $(".openItem").click(function(){
+//         $(this).find('.fa-bars').toggleClass("hidden");
+//         $(this).find('.fa-remove').toggleClass("hidden");//.css({"color":"#fff"})
+//         //全螢幕與手機螢幕畫面
+//         $(this).parents('header').siblings('.open_ul').toggleClass("show");
+//     })
+// });
+
+$(function(){
+    $('.menu').click(function(){
+        $('.bar').toggleClass('animate').siblings('.open_ul');
         $(this).parents('header').siblings('.open_ul').toggleClass("show");
     })
 });
-
-
 
